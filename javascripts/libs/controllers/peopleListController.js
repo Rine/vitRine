c238ba490b212ce36aa8646e78e6c5308f2c9d6c
Rine <rine.miquet@gmail.com>
2013-11-17 17:53:51 +0100

    drag & drop 2nd essai

--- javascripts/libs/controllers/peopleListController.js
+++
@@ -1,9 +0,0 @@
-app.controller('PeopleListController', ['$scope', 'Network', '$location', function($scope, $network, $location) {
-
-
-    $network.getPeopleList ( function(peopleList) {
-	   $scope.peopleList = peopleList;
-    })
-
-
-}]);
