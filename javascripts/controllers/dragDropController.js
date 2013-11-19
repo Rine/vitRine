d3ab8688e13805279eb116751565a009fb6c60bf
Rine <rine.miquet@gmail.com>
2013-11-19 08:27:18 +0100

    ng-repeat+drag & drop

--- javascripts/controllers/dragDropController.js
+++ javascripts/controllers/dragDropController.js
@@ -1,5 +1,13 @@
 app.controller('dragDropController', ['$scope', function($scope) {
 
+ $scope.parcours = [
+ 
+{"title":"veni", "article":"je suis venue"},
+ 
+{"title":"vedi", "article":"j'ai vu"},
+ 
+{"title":"vici","article":"j'ai vaincu"}]; 
+
 
     $scope.handleDrop = function() {
        alert('Bomb has been planted');
